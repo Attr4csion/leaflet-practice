667d0d0d35d6a5557b85c0bfd48cea5a38f88dcd
Attr4csion <Attr4csion@yandex.ru>
2024-01-11 18:21:54 +0500

    #1

--- src/components/SightsDetail.vue
+++ src/components/SightsDetail.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="w-[90%] h-[90%] mx-auto flex gap-20">
+    <el-button @click="router.push('/')">Назад</el-button>
     <div class="flex flex-col gap-5 w-[700px] p-10 h-fit bg-gray-100 rounded">
-      <img :src="currentSight?.image" />
+      <img :src="currentSight?.image" class="image"/>
       <p>{{ currentSight?.name }}</p>
       <p>
         {{ currentSight?.description }}
@@ -15,7 +16,15 @@
 import { useRoute } from 'vue-router';
 import sight from '@/assets/sight.json';
 import TheMap from '../components/TheMap.vue';
+import router from '@/router';
 
 const { params } = useRoute();
 const currentSight = sight.find((el) => el.id == params.id);
 </script>
+
+<style scoped>
+.image {
+  width: 100%;
+  display: block;
+}
+</style>
